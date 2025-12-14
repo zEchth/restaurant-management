b8a2267d9d4ff648eea9d89341a25788048618db
zEchth <fauzanfath099@gmail.com>
2025-12-14 11:04:08 +0800

    perbaiki production app.js

--- src/app.js
+++ src/app.js
@@ -11,10 +11,12 @@ const categoryRoutes = require('./routes/categoryRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
-const path = require('path');
 
 // Global Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'http://100.28.231.115',
+}));
+app.use(express.json());
 app.use(express.json()); // Parsing body JSON
 
 // Logger hanya di Development
@@ -40,14 +42,14 @@ app.get('/api/health', (req, res) => {
 });
 
 // --- SERVE FRONTEND (STATIC FILES) ---
-const frontendPath = path.join(__dirname, '../../restaurant-frontend/dist');
-app.use(express.static(frontendPath));
+// const frontendPath = path.join(__dirname, '../../restaurant-frontend/dist');
+// app.use(express.static(frontendPath));
 
 // 2. Handle React Routing (SPA)
 // Jika route tidak dikenali API, kirimkan index.html React
-app.use((req, res) => {
-  res.sendFile(path.join(frontendPath, 'index.html'));
-});
+// app.use((req, res) => {
+//   res.sendFile(path.join(frontendPath, 'index.html'));
+// });
 
 // 404 Handler (Route Not Found)
 app.use((req, res, next) => {
